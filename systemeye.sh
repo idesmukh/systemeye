50fcceaaa3c1f609d44f1f0a4c811661b7a56f33
Ibad Desmukh <ibaddesmukh@gmail.com>
2025-04-01 15:41:51 +0000

    Add total memory usage monitoring feature

--- systemeye.sh
+++ systemeye.sh
@@ -20,16 +20,34 @@ readonly PRINT_CPU_USAGE='
   printf "%.1f%%\n", cpu_usage
 }'
 
+# AWK script to extract memory usage and print it.
+# Stored in a read-only variable for reuse.
+readonly PRINT_MEMORY_USAGE='
+/^Mem:/ {
+  memory_total = $2
+  memory_used = $3
+  memory_used_percent = (memory_used / memory_total) * 100
+  memory_available = $7
+  memory_available_percent = (memory_available / memory_total) * 100
+  printf "Total: %s Used: %s (%.1f%%) Available: %s (%.1f%%)\n", memory_total, memory_used, memory_used_percent, memory_available, memory_available_percent
+}'
+
 while true; do
   # Refresh screen.
   clear
 
   # Print basic headings for usability.
   printf "Systemeye\n"
+
   printf "#### Total CPU Usage ####\n"
 
   # Run top, pass output to AWK, and run AWK script.
   top -b -n 1 | awk "${PRINT_CPU_USAGE}"
+
+  printf "#### Total Memory Usage ####\n"
+
+  # Run free, pass output to AWK, and run AWK script.
+  free -h | awk "${PRINT_MEMORY_USAGE}"
 
   # Wait for five seconds before refresh.
   sleep 5
